95b62156f8e3e0eee55134c590de346c811503d9
vky <gillv625@gmail.com>
2022-05-31 14:17:40 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,37 @@
+window.onscroll = function () { scrollFunction() };
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".sidebar > div");
+let current = "";
+
+function scrollFunction() {
+
+    //cambio en navbar
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop >100) {
+        document.querySelector(".navbar").classList.remove("expanded")
+    } else {
+        document.querySelector(".navbar").classList.add("expanded")
+    }
+
+    current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (window.pageYOffset >= sectionTop - 180) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLi.forEach((li) => {
+        li.classList.remove("selected");
+        if (li.classList.contains("nav-" + current)) {
+            li.classList.add("selected");
+        }
+    });
+}
+
+
+//to remove
+
+window.setTimeout(function () { 
+    window.scrollTo(0, 1725);
+}, 700)
