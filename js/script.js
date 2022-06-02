8d6c81917df3d8b5a5bcfac5aeadd590473175f9
vky <gillv625@gmail.com>
2022-06-02 14:28:57 +0200

    acabado

--- js/script.js
+++ js/script.js
@@ -1,26 +1,21 @@
 window.onscroll = function () { scrollFunction() };
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll(".sidebar > div");
-let current = "";
-
-function scrollFunction() {
-
+let current = ""; 
+function scrollFunction() { 
     //cambio en navbar
     if ((document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) && window.screen.width > 768) {
         document.querySelector(".navbar").classList.remove("expanded")
     } else {
         document.querySelector(".navbar").classList.add("expanded")
-    }
-
-    current = "";
-
+    } 
+    current = ""; 
     sections.forEach((section) => {
         const sectionTop = section.offsetTop;
         if (window.pageYOffset >= sectionTop - 180) {
             current = section.getAttribute("id");
         }
-    });
-
+    }); 
     navLi.forEach((li) => {
         li.classList.remove("selected");
         if (li.classList.contains("nav-" + current)) {
@@ -30,8 +25,4 @@ function scrollFunction() {
 }
 
 
-//to remove
-
-// window.setTimeout(function () {
-//     window.scrollTo(0, 1725);
-// }, 700)
+ 
